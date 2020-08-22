847abcf4b4a3dac72cbf43bf130732b921f34a9b
narumi817 <s2cartoon817@gmail.com>
2020-08-22 19:50:09 +0900

    add RectangleApp

---
+++ RectangleApp/RectangleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RectangleApp
+//
+//  Created by 水越成美 on 2020/08/22.
+//  Copyright © 2020 水越成美. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Rectangle().padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
